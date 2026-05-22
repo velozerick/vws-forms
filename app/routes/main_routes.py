5d636af80263bc98e20e639343e2b90b6b6fa809
velozerick <veloxer25@outlook.com>
2026-05-22 14:01:37 -0600

    GET POST added

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -1,15 +1,25 @@
 #Importamos blueprint para crear grupo de rutas y render para mostrar archivos html
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request #request es la peticion que llega
 
 #Creamos el grupo de rutas principal
 main = Blueprint("main", __name__)
 
 
 #Ruta principal del sitio
-@main.route("/")
+@main.route("/",methods=["GET","POST"]) #Esta ruta acepta GET y POST
 
 def home():
     #Mostramos el html
+
+    #verificamos si el usuario envio el formulario
+    if request.method == "POST":
+
+        #Obtener dato, desde el formulario
+        negocio = request.form.get("negocio")
+        
+        #Mostrar dato en temrinal
+        print(f"Negocio: {negocio}")
     return render_template ("home.html")
+
 
 
